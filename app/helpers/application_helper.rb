79b9ca26197713c21c4706e386ddb0d8b2c7123c
Andy Smith <andy@37signals.com>
2025-06-19 13:21:32 -0500

    WIP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,11 @@ module ApplicationHelper
   def icon_tag(name, **options)
     tag.span class: class_names("icon icon--#{name}", options.delete(:class)), "aria-hidden": true, **options
   end
+
+  def circled_text(text, **options)
+    tag.mark(class: class_names("circled-text", options.delete(:class)), "aria-hidden": true, **options) do
+      concat text
+      concat(tag.span)
+    end
+  end
 end
